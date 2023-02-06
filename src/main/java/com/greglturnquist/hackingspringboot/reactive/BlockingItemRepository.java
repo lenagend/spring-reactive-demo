643db3f2643d7e8873322ee17ba2cb15b8ec7eb4
lenagend <lenagend@naver.com>
2023-02-06 17:11:35 +0900

    20230206

---
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/BlockingItemRepository.java
@@ -0,0 +1,8 @@
+/*
+package com.greglturnquist.hackingspringboot.reactive;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlockingItemRepository extends CrudRepository<Item, String> {
+}
+*/
---
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/RepositoryDatabaseLoader.java
@@ -0,0 +1,19 @@
+/*
+package com.greglturnquist.hackingspringboot.reactive;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RepositoryDatabaseLoader {
+
+    @Bean
+    CommandLineRunner initialize(BlockingItemRepository repository){
+        return args ->{
+            repository.save(new Item("갤럭시 s23", 1140000));
+            repository.save(new Item("아이폰 14프로", 1360000));
+        };
+    }
+}
+*/
